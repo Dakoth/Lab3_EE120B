c4b8015cf09047a6733cedf76d44e6858998e1c4
Alfredo Gonzalez <agonz250@wch136-11.cs.ucr.edu>
2020-04-09 23:12:52 -0700

    edited main

--- source/main.c
+++ source/main.c
@@ -1,7 +1,7 @@
 /*	Author: agonz250
- *  Partner(s) Name: 
- *	Lab Section:
- *	Assignment: Lab #  Exercise #
+ *  Partner(s) Name:  
+ *	Lab Section: 028
+ *	Assignment: Lab #3  Exercise # 1
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
@@ -13,11 +13,20 @@
 #endif
 
 int main(void) {
-    /* Insert DDR and PORT initializations */
+    	/* Insert DDR and PORT initializations */
+	DDRA = 0x00; PORTA = 0xFF;	//Makes all port A's input pins as inputs; initalized to 1s 
+	DDRB = 0x00; PORTB = 0xFF; 	// similar to port A but for B
+	DDRC = 0xFF; PORTC = 0x00;	//Makes all port C's input pins as outputs; initalized to 0s
 
-    /* Insert your solution below */
-    while (1) {
+	unsigned char tmpA, tmpB, tmpC;
 
-    }
-    return 1;
+
+	/* Insert your solution below */
+    	while (1) {
+		//1.)read input
+		//2.) perform computation 
+		//3.) Write Output 
+
+    	}
+    	return 1;
 }
